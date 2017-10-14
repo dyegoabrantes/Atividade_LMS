b8955c3e26c75099ec0d8f8f3e782230407a28bc
dyegoabrantes <dyegoabrantes@gmail.com>
2017-10-14 13:21:30 -0300

    atividade 5

---
+++ atividade5.js
@@ -0,0 +1,18 @@
+
+
+var acc = document.getElementsByClassName("botao_menu");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+var x = document.getElementsByClassName("acordeon_conteudo")
+console.log(x.color)
