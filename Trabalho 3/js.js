b7667f3d7de54aa3d8b14e31df038bdc7e6f0c54
dyegoabrantes <dyegoabrantes@gmail.com>
2017-12-04 23:35:32 -0300

    atualização t#

--- Trabalho 3/js.js
+++ Trabalho 3/js.js
@@ -1,6 +1,6 @@
 	//verificar usuário logado
 		function verUser(){
-			if (localStorage.user!="") {
+			if (localStorage.userBrasil!="") {
 				$(".cadastro").toggleClass("hidden") ;
 				$(".login").toggleClass("hidden");
 				$(".sair").toggleClass("hidden");
@@ -9,19 +9,7 @@
 			}
 		}
 	verUser();
-
-	// Carregar usuários cadastrados:
-	let users = [];
-	function getUsers(){
-		$.ajax({
-			type: 'GET',
-			url: 'http://rest.learncode.academy/api/pedrasbrasil/users',
-			success: function(data){
-				users = data;
-			}
-		});
-	};
-	getUsers();
+	// retrieveCart()
 	
 	//captura formulário
 	let username = "";
@@ -31,7 +19,7 @@
 	function validar(){
 		let permite = true;
 		for (var i = 0; i <users.length; i++) {
-			if (username==users[i].user) {
+			if (username==users[i].userBrasil) {
 				permite = false;
 				break;
 			}
@@ -48,7 +36,7 @@
 		$.ajax ({
 			type:'POST',
 			url:'http://rest.learncode.academy/api/pedrasbrasil/users',
-			data:{user:username, password:userPassword},
+			data:{userBrasil:username, password:userPassword},
 			dataType: 'json',
 			success: function(data){
 				alert("Usuário Cadastrado! Entre com seu usuário e senha!")
@@ -70,24 +58,33 @@
 
 	let permite = false;
 	function validaLogin(a,b){
-		for (var i = 0; i <users.length; i++) {
-			if (a==users[i].user || b==users[i].password) {
-				permite = true;
-				break;
+		let users = [];
+		$.ajax({
+			type: 'GET',
+			url: 'http://rest.learncode.academy/api/pedrasbrasil/users',
+			success: function(data){
+				let users = data;
+				for (var i = 0; i <users.length; i++) {
+					if (a==users[i].userBrasil || b==users[i].password) {
+						permite = true;
+						break;
+					}
+				}
+				if (permite == true) {
+					localStorage.userBrasil=a;
+					$(".cadastro").toggleClass("hidden") ;
+					$(".login").toggleClass("hidden");
+					$("#inputUsuario").val("");
+					$("#inputPasword").val("");
+					$(".buy").toggleClass("hidden");
+					$(".sair").toggleClass("hidden");
+					$(".botaoFinalizar").toggleClass("hidden");
+					alert("Login Efetuado!");
+				}else{
+					alert("Deu ruim, usuário ou senha incorreta")
+				}
 			}
-		}
-		if (permite == true) {
-			localStorage.user=a;
-			$(".cadastro").toggleClass("hidden") ;
-			$(".login").toggleClass("hidden");
-			$("#inputUsuario").val("");
-			$("#inputPasword").val("");
-			$(".buy").toggleClass("hidden");
-			$(".sair").toggleClass("hidden");
-			$(".botaoFinalizar").toggleClass("hidden");
-		}else{
-			alert("Deu ruim, usuário ou senha incorreta")
-		}
+		});
 	}
 
 	let inputUsuario = "";
@@ -97,7 +94,6 @@
 			inputUsuario = $("#inputUsuario").val();
 			inputPasword = $("#inputPasword").val();
 			validaLogin(inputUsuario,inputPasword);
-			alert("Login Efetuado!");
 		})
 
 	//deslogar
@@ -105,27 +101,44 @@
 	$(".sair").click(function(event){
 		event.preventDefault();
 		permite = false;
-		localStorage.user="";
+		localStorage.userBrasil="";
 		$(".cadastro").toggleClass("hidden") ;
 		$(".login").toggleClass("hidden");
 		$(".sair").toggleClass("hidden");
 		$(".botaoFinalizar").toggleClass("hidden");
 		$(".buy").toggleClass("hidden");
+		window.location.href='galeria.html';
 	})
 
 	//Adicionar a lista
+
+	let products = new Array;
 	$(".adccart").click(function(){
-		let botaoFinalizar = $(".botaoFinalizar"); 
-		let produtoImagem = ($(this).offsetParent().find(".card-image-top").attr("src"));
-		let produtoNome = ($(this).offsetParent().find(".card-title").text());
-		let produtoValor = ($(this).offsetParent().find(".valor").text());
-console.log(produtoImagem)
-		// var image = $("<div></div>").addClass("col-md-3").append(produtoImagem);
-		// var nomP = $("<div></div>").addClass("col-md-3").append(produtoNome);
-		// var valP = $("<div></div>").addClass("col-md-3").append("R$",produtoValor);
+		let item = {
+			nome: $(this).offsetParent().find(".pName").text(),
+			valor: $(this).offsetParent().find(".valor").text(),
+			quantidade: $(this).offsetParent().find("#quantidade").val(),
+			imagem_src: $(this).offsetParent().find(".pImage").attr('src')
+		};
+		products.push(item);
+		localStorage.clear();
+		localStorage.setItem("products", JSON.stringify(products));
+	});
 
+	$(".minhas-compras").click(function(){
+		let productsLocalStorage = JSON.parse(localStorage.getItem("products"));
+		$(".carro").empty();
+		if (productsLocalStorage.length == 0) {
+			$(".carro").append('<p id="textoA">Você ainda não adicionou nenhum produto :(</p>');
+		} else {
+			for (i = 0; i < productsLocalStorage.length; i++) {
+				var lil = $('<div class="lilcard"><img width="50px" height="50px" class="foto" src="'
+					+productsLocalStorage[i].imagem_src+'"><span class="nomeP">'
+					+productsLocalStorage[i].nome+'</span><span class=" valorP">'
+					+"R$"+productsLocalStorage[i].valor+'</span><span class="quantidadeP">'
+					+"Quantidade: "+productsLocalStorage[i].quantidade+'</span></div>');
+				$(".carro").append(lil);
+			}
+		}
+	});
 
-		// var cartao = $("<div></div>").addClass("row cartao").append(image, nomP, valP);
-
-		// $(".carro").append(cartao)
-	})
